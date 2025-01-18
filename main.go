d8262c94714774ab707a3f09b9b649b7e5485c1c
Mahesh <mahimali15082001@gmail.com>
2025-01-19 00:46:26 +0530

    fix(ticket-system): refactor code to be moduler and extensible

--- main.go
+++ main.go
@@ -5,42 +5,24 @@ import (
 	"strings"
 )
 
-func main() {
-	const conferenceTickets int = 50
-	remainingTickets := conferenceTickets
-	ticketBuyers := []string{}
-	ticketBoughts := []int{}
+const conferenceTickets int = 50
 
-	fmt.Println("Welcome to Go programming conference!")
-	fmt.Printf("We have total of %v tickets for conference\n", conferenceTickets)
+var remainingTickets = conferenceTickets
+var ticketBuyers = []string{}
+var ticketBoughts = []int{}
+
+func main() {
+	greetUsers()
 
 	for {
 		fmt.Printf("There are %v tickets remaining, Hurry up!!\n", remainingTickets)
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets int
+		firstName, lastName, email, userTickets := getUserInputs()
 
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
-		fmt.Println("Enter your email address: ")
-		fmt.Scan(&email)
-		fmt.Println("Enter the number of tickets you want to book: ")
-		fmt.Scan(&userTickets)
-
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := email != "" && strings.Contains(email, "@")
-		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInputs(firstName, lastName, email, userTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			remainingTickets = remainingTickets - userTickets
-			ticketBuyers = append(ticketBuyers, firstName+" "+lastName)
-			ticketBoughts = append(ticketBoughts, userTickets)
-			fmt.Printf("You have booked %v tickets\n", userTickets)
+			bookTickets(firstName, lastName, userTickets)
 		} else {
-
 			if !isValidName {
 				fmt.Println("First name or last name you entered is too short")
 			}
@@ -62,10 +44,51 @@ func main() {
 		}
 	}
 
+	conferenceBookingEndingResponse()
+
+}
+
+func greetUsers() {
+	fmt.Println("Welcome to Go programming conference!")
+	fmt.Printf("We have total of %v tickets for conference\n", conferenceTickets)
+}
+
+func getUserInputs() (string, string, string, int) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter the number of tickets you want to book: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func validateUserInputs(firstName string, lastName string, email string, userTickets int) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := email != "" && strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func bookTickets(firstName string, lastName string, userTickets int) {
+	remainingTickets = remainingTickets - userTickets
+	ticketBuyers = append(ticketBuyers, firstName+" "+lastName)
+	ticketBoughts = append(ticketBoughts, userTickets)
+	fmt.Printf("Thank you %v for booking %v tickets.\n", firstName+" "+lastName, userTickets)
+}
+
+func conferenceBookingEndingResponse() {
 	fmt.Println("Conference ticket booking is over!")
 	fmt.Println("Welcome gophers to conference!")
 	for index, value := range ticketBuyers {
 		fmt.Printf("Gopher : %v has booked %v tickets...\n", value, ticketBoughts[index])
 	}
-
 }
